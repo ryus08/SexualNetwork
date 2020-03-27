40d835c9405ac5dfce1ed3fee44ccddbfe89c0dc
Jamie Cohen <jamie.alex.cohen@gmail.com>
2020-03-27 19:22:24 -0400

    zach check this commit, lines 68 - 76

--- classes.py
+++ classes.py
@@ -11,6 +11,7 @@ class PartnershipType(Enum):
     MARITAL = 1
     SHORT_TERM = 2
     CASUAL = 3
+    INSTANTANEOUS = 4
 
 class Individual:
 
@@ -38,6 +39,8 @@ class Individual:
     def create_partnership(self, men):
         for m in men:
             if m.single:
+                rand = random.random()
+                ## add in partnership type separation here
                 self.add_partner(m.id, 1, PartnershipType.CASUAL)
                 self.single = False
                 m.numpartners += 1
@@ -62,6 +65,15 @@ class Individual:
 
     def check_relationships(self, men):
         for partnerid, partnership in self.partners.items():
+            if partnership[1] == PartnershipType.MARITAL:
+                pass
+            elif partnership[1] == PartnershipType.CASUAL:
+                pass
+            elif partnership[1] == PartnershipType.SHORT_TERM:
+                pass
+            elif partnership[1] == PartnershipType.INSTANTANEOUS:
+                pass
+
             if partnership[0] < self.maxdur:
                 dur = partnership[0] + 1
                 self.partners[partnerid] = (dur, partnership[1])
